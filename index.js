c839dbad60c77d8c40196cd6b58507593ae14f16
Andrew Dina <andrewthedina@icloud.com>
2020-11-28 07:52:51 -0500

    removes grid classes and ensures pacman cannot pass through grid border

--- index.js
+++ index.js
@@ -40,15 +40,15 @@ function createBoard() {
         grid.appendChild(square)
         squares.push(square)
 
-        if (layout[i] === 0) {
-            squares[i].classList.add('pac-dot')
-        } else if (layout[i] === 1) {
-            squares[i].classList.add('wall')
-        } else if (layout[i] === 2) {
-            squares[i].classList.add('ghost-lair')
-        } else if (layout[i] === 3) {
-            squares[i].classList.add('power-pellet')
-        }
+        // if (layout[i] === 0) {
+        //     squares[i].classList.add('pac-dot')
+        // } else if (layout[i] === 1) {
+        //     squares[i].classList.add('wall')
+        // } else if (layout[i] === 2) {
+        //     squares[i].classList.add('ghost-lair')
+        // } else if (layout[i] === 3) {
+        //     squares[i].classList.add('power-pellet')
+        // }
     }
 }
 
@@ -59,12 +59,14 @@ let pacmanCurrentIndex = 490
 squares[490].classList.add('pac-man')
 
 function movePacMan(event) {
+    let nextIndex
+    console.log(pacmanCurrentIndex)
     switch(event.keyCode) {
         // move down
         case 40:
-        if(!squares[pacmanCurrentIndex + 28].classList.contains('wall')) {
-            adjustPacMan(28)
-            
+        nextIndex = pacmanCurrentIndex + 28
+        if(nextIndex < width * width) {
+            adjustPacMan(width)
         } else {
             console.log('contains wall')
         }
@@ -72,8 +74,9 @@ function movePacMan(event) {
         
         // move up
         case 38:
-        if(!squares[pacmanCurrentIndex - 28].classList.contains('wall')) {
-            adjustPacMan(-28)
+        nextIndex = pacmanCurrentIndex - width
+        if(nextIndex > -1) {
+            adjustPacMan(-width)
         } else {
             console.log('contains wall')
         }
@@ -82,7 +85,8 @@ function movePacMan(event) {
         
         // move left
         case 37:
-        if(!squares[pacmanCurrentIndex - 1].classList.contains('wall')) {
+        nextIndex = pacmanCurrentIndex - 1
+        if(nextIndex % width >= 0) {
             adjustPacMan(-1)
         } else {
             console.log('contains wall')
@@ -91,13 +95,55 @@ function movePacMan(event) {
 
         // move right
         case 39:
-        if(!squares[pacmanCurrentIndex - 1].classList.contains('wall')) {
+        nextIndex = pacmanCurrentIndex + 1
+        if(nextIndex % width != 0) {     
             adjustPacMan(1)
         } else {
-            console.log('contains wall')
+            console.log('rcontains wall')
         }
         break
     }
+
+
+    // switch(event.keyCode) {
+    //     // move down
+    //     case 40:
+    //     if(!squares[pacmanCurrentIndex + 28].classList.contains('wall')) {
+    //         adjustPacMan(28)
+            
+    //     } else {
+    //         console.log('contains wall')
+    //     }
+    //     break
+        
+    //     // move up
+    //     case 38:
+    //     if(!squares[pacmanCurrentIndex - 28].classList.contains('wall')) {
+    //         adjustPacMan(-28)
+    //     } else {
+    //         console.log('contains wall')
+    //     }
+        
+    //     break
+        
+    //     // move left
+    //     case 37:
+    //     if(!squares[pacmanCurrentIndex - 1].classList.contains('wall')) {
+    //         adjustPacMan(-1)
+    //     } else {
+    //         console.log('contains wall')
+    //     }
+    //     break
+
+    //     // move right
+    //     case 39:
+    //     if(!squares[pacmanCurrentIndex - 1].classList.contains('wall')) {
+    //         adjustPacMan(1)
+    //     } else {
+    //         console.log('contains wall')
+    //     }
+    //     break
+    // }
 }
 
 function adjustPacMan(amount) {
